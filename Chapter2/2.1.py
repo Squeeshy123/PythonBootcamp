92c7028d9866b6b11e5f007d61eed4e0fee4bcb2
Squeeshy123 <62818641+Squeeshy123@users.noreply.github.com>
2021-06-03 16:27:07 -0400

    Working on chapter 2

---
+++ Chapter2/2.1.py
@@ -0,0 +1,15 @@
+# Chapter 2: Loops and flow control
+
+some_bool = True
+
+if some_bool:
+    print("The Boolean is true!")
+
+class SomeObject:
+    some_data = 0
+
+obj = SomeObject()
+if obj:
+    print("Some object is true!")
+
+
